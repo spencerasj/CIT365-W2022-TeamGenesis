70e380ca17c75ac19b9692226f33480635f50d14
CaylaTribett <wil08010@byui.edu>
2022-02-01 03:25:59 -0500

    Added reading rush order Prices from a txt file.

--- WindowsFormsApp1/DeskQuote.cs
+++ WindowsFormsApp1/DeskQuote.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace MegaDesk2
 {
-     public class DeskQuote
+    public class DeskQuote
     {
         private Desk desk;
         private int rushDays;
@@ -33,7 +34,7 @@ namespace MegaDesk2
             {7, 40}
 
         };
-        public static int [] rushOrderCost = { };
+        public static int[] rushOrderCost = { };
 
         /*
         public DeskQuote(Desk desk, string customerName, int rushDays)
@@ -48,22 +49,84 @@ namespace MegaDesk2
         {
             quoteDate = DateTime.Now;
         }
-        
 
+        private Array GetRushOrder()
+        {
+            int[,] rushOrder = new int[3, 3];
+            try
+            {
+                string [] numbers = File.ReadAllLines(@"Data\rushOrderPrices.txt");
+                int next = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int t = 0; t < 3; t++)
+                    {
+
+                        rushOrder[i, t] = Int32.Parse((string)numbers.GetValue(next));
+                        next += 1;
+
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ERROR: File could not be found");
+            }
+            return rushOrder;
+
+        }
         public int rushOrderAdditionalCosts(int surfaceArea)
         {
-            int additionalRushOrderCost = 0;
-            if(surfaceArea < 1000)
-            {
-                additionalRushOrderCost += LessThanThousand[rushDays];
-            }else if (surfaceArea > 1000 && surfaceArea < 2000)
-            {
-                additionalRushOrderCost += BetweenThousandAndTwoThousand[rushDays];
-            }else
-            {
-                additionalRushOrderCost += OverTwoThousand[rushDays];
+            Array rushOrder = GetRushOrder();
+
+            if (surfaceArea< 1000){
+                switch (rushDays)
+                {
+                    case 3:
+                        return (int)rushOrder.GetValue(0,0);
+
+                    case 5:
+                        return (int)rushOrder.GetValue(1, 0);
+
+                    case 7:
+                        return (int)rushOrder.GetValue(2, 0);
+
+                    default: return 0;
+                }
             }
-            return additionalRushOrderCost;
+            else if (surfaceArea > 1000 && surfaceArea < 2000)
+            {
+                switch (rushDays)
+                {
+                    case 3:
+                        return (int)rushOrder.GetValue(0, 1);
+
+                    case 5:
+                        return (int)rushOrder.GetValue(1, 1);
+
+                    case 7:
+                        return (int)rushOrder.GetValue(2, 1);
+
+                    default: return 0;
+                }
+            }
+            else
+            {
+                switch (rushDays)
+                {
+                    case 3:
+                        return (int)rushOrder.GetValue(0, 2);
+
+                    case 5:
+                        return (int)rushOrder.GetValue(1, 2);
+
+                    case 7:
+                        return (int)rushOrder.GetValue(2, 2);
+
+                    default: return 0;
+                }
+            }
+
         }
 
         public int rushOrderAdditionalCosts()
@@ -90,7 +153,7 @@ namespace MegaDesk2
             int surfaceArea = desk.Width * desk.Depth;
             if(surfaceArea > 1000)
             {
-                price += (surfaceArea - 100);
+                price += (surfaceArea - 1000);
             }
             price += (desk.NumberOfDrawers * 50);
             price += (int)desk.SurfaceMaterial;
@@ -112,7 +175,7 @@ namespace MegaDesk2
             //        price += 125;
             //        break;
             //}
-            if (rushDays > 0)
+            if (rushDays < 14)
             {
                 price += rushOrderAdditionalCosts(surfaceArea);
             }
@@ -132,13 +195,13 @@ namespace MegaDesk2
         {
             get { return rushDays; }
             set { 
-                    if (value == 0 || value == 3 || value == 5 || value == 7)
+                    if (value == 14 || value == 3 || value == 5 || value == 7)
                     {
                         rushDays = value;
                     }
                     else
                     {
-                    throw (new Exception("Needs to be a Number 0, 3, 5 or 7"));
+                    throw (new Exception("Needs to be a Number  3, 5, 7 or 14"));
                     }
                 }
         }
