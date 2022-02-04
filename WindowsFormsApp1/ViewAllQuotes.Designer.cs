eb76ad85ac282e67d95091083ff0a0f8a0c00a69
Erika <erikacardon@gmail.com>
2022-02-03 22:00:52 -0800

    Fixed view all quotes display

--- WindowsFormsApp1/ViewAllQuotes.Designer.cs
+++ WindowsFormsApp1/ViewAllQuotes.Designer.cs
@@ -32,10 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.settingsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.resourcesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.resourcesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.settingsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).BeginInit();
@@ -65,11 +66,16 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
-            this.Column3});
+            this.Column3,
+            this.Column4});
             this.dataGridView1.Location = new System.Drawing.Point(12, 54);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(685, 291);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // resourcesBindingSource
+            // 
+            this.resourcesBindingSource.DataSource = typeof(MegaDesk2.Properties.Resources);
             // 
             // Column1
             // 
@@ -83,12 +89,13 @@
             // 
             // Column3
             // 
-            this.Column3.HeaderText = "Quote Total";
+            this.Column3.HeaderText = "Surface Material";
             this.Column3.Name = "Column3";
             // 
-            // resourcesBindingSource
+            // Column4
             // 
-            this.resourcesBindingSource.DataSource = typeof(MegaDesk2.Properties.Resources);
+            this.Column4.HeaderText = "Quote Total";
+            this.Column4.Name = "Column4";
             // 
             // ViewAllQuotes
             // 
@@ -115,9 +122,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.BindingSource settingsBindingSource;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource resourcesBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.BindingSource resourcesBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     }
 }
